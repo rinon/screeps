1cc344994c7b99cd03692c512362ac27cdcc0821
Multitallented <multitallented@gmail.com>
2018-08-03 21:40:41 -0700

    wip on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
 'use strict';
 
-const gutil = require('gulp-util');
 const runSequence = require('run-sequence');
 const clean = require('gulp-clean');
 const gulp = require('gulp');
 const gulpDotFlatten = require('./libs/gulp-dot-flatten.js');
-const gulpRename = require('gulp-rename');
-const path = require('path');
-const PluginError = require('gulp-util').PluginError;
-const buildTarget = "";
-
-/*********/
-/* TASKS */
-/*********/
 
 gulp.task('clean', function () {
-    return gulp.src(['dist/tmp/', 'dist/' + buildTarget], { read: false, allowEmpty: true })
+    return gulp.src(['dist/tmp/', 'dist/'], { read: false, allowEmpty: true })
         .pipe(clean());
 });
 
@@ -26,24 +17,10 @@ gulp.task('compile-flattened', function() {
 });
 
 gulp.task('compile', function() {
-    runSequence('clean', 'compile-flattened', function() {
-        //
-    });
+    runSequence('clean', 'compile-flattened');
 });
 
 gulp.task('watchLocal', function () {
-    gulp.watch('src/**/*.js', ['compile'])
-        .on('all', function(event, path, stats) {
-            console.log('');
-            gutil.log(gutil.colors.green('File ' + path + ' was ' + event + 'ed, running tasks...'));
-        })
-        .on('error', function () {
-            gutil.log(gutil.colors.green('Error during build tasks: aborting'));
-        });
-});
-
-gulp.task('build', function buildDone(done) {
-    gutil.log(gutil.colors.green('Build done'));
-    return done();
+    gulp.watch('src/**/*.js', ['compile']);
 });
 gulp.task('default', ['compile', 'watchLocal']);
