b41cffd21fad992eceff1bdbbc9441d46d8f8830
Stephen Crane <sjc@immunant.com>
2020-03-01 23:05:56 -0800

    Add prototype to Room earlier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { CreepController } from "./creeps/creep-controller";
 import { RoomController } from "./room/room-controller";
+import { RoomPrototype } from "./room/room-prototype";
 
 module.exports = {
     loop: function() {
+        RoomPrototype.init();
         for (var name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
