fe7274b8de8a4f5cdcf2705fe8d6a12f6895e93e
Stephen Crane <sjc@immunant.com>
2020-03-01 21:17:46 -0800

    Add room prototype

---
+++ src/room/room-prototype.ts
@@ -0,0 +1,9 @@
+const findNextEnergySource = function() {
+    
+}
+
+export class RoomPrototype {
+    static init() {
+        Room.prototype.findNextEnergySource = findNextEnergySource;
+    }
+}
