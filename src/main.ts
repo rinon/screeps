d9f31536b65a2924e01b65b56ee5c947ea0924a6
Stephen Crane <sjc@immunant.com>
2020-03-01 20:52:39 -0800

    Remove everything

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
-import {Respawn} from "./respawn";
 import * as _ from "lodash";
 
-const roleUpgrader = require('./roles/role.upgrader');
 module.exports = {
     loop: function() {
-
         for(var name in Memory.creeps) {
             if(!Game.creeps[name]) {
                 delete Memory.creeps[name];
             }
         }
-
-        Respawn.run();
-
-        _.forEach(Game.creeps, (creep) => {
-            if(creep.memory['role'] === 'upgrader') {
-                roleUpgrader.run(creep);
-            }
-        });
     }
 };
